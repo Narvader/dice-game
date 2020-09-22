3547466151a860c920aa74b5eadf15ce78eddd27
Pablo Narvaez <pnarvaez@elconfidencial.com>
2020-09-22 10:05:57 +0200

    Updates

---
+++ precache-manifest.d00adf88d3cd344128cc5def21f4a951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4ce1a7f36519dfdf2d6f8ce9648625",
+    "url": "/dice-game/index.html"
+  },
+  {
+    "revision": "21bbde48c23cd729b76b",
+    "url": "/dice-game/static/css/main.4b639976.chunk.css"
+  },
+  {
+    "revision": "ed7a55a51a1d0159e3b9",
+    "url": "/dice-game/static/js/2.1703f224.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-game/static/js/2.1703f224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21bbde48c23cd729b76b",
+    "url": "/dice-game/static/js/main.a501f7f0.chunk.js"
+  },
+  {
+    "revision": "271f9ea52251b5fa5ffe",
+    "url": "/dice-game/static/js/runtime-main.f67d8e22.js"
+  }
+]);
